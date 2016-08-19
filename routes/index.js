0787c1a86a77ef195fb9c0ae80ad31a042f0ff30
haiyan.sun <haiyan.sun@servision.com.cn>
2016-08-19 10:06:48 +0800

    window step one

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/combobox', function(req, res, next) {
 	res.render('combobox.ejs');
 });
 
+router.get('/window', function(req, res, next) {
+	res.render('window.ejs');
+});
+
 module.exports = router;
