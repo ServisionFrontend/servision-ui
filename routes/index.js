468e8adede0e30ee5f5468490d8c963227f35317
lihua.qiu <lihua.qiu@servision.com.cn>
2016-08-19 10:22:50 +0800

    调整ejs 模板映射

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
+	res.render('index.ejs');
+});
+
+router.get('/textbox', function(req, res, next) {
+	res.render('textbox.ejs');
+});
+
+router.get('/tree', function(req, res, next) {
 	res.render('treeview.ejs');
 });
 
