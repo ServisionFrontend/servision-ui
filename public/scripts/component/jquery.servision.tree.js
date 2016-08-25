e56ffd954a504dff87eb0f583ceaddbc526c5fab
lihua.qiu <lihua.qiu@servision.com.cn>
2016-08-25 09:55:32 +0800

    树添加checkbox功能

--- public/scripts/component/jquery.servision.tree.js
+++ public/scripts/component/jquery.servision.tree.js
@@ -13,16 +13,15 @@
 			var self = this,
 				opts = $.data(target, 'tree').options,
 				url = opts.url,
-				data = opts.data,
-				nodePathList = opts.nodePathList;
+				data = opts.data;
 
 			self.initEvents(target);
 
-			if (data) {
-				self.buildData(data, 0, [], nodePathList);
-				self.render(data, target, opts);
+			if (data.length) {
+				self.buildData(data, 0, [], opts.nodePathList);
+				self.render(target, data);
 			} else if (url) {
-				self.loadData(url);
+				self.loadData(target, url);
 			}
 		},
 
@@ -38,14 +37,20 @@
 				$(this).removeClass('tree-node-hover');
 			});
 
-			// 选择节点行
-			$(target).on('click', '.tree-icon, .tree-text', function(e) {
-				self.selectRow(this, target);
+			// 节点行事件监听
+			$(target).on('click', '.tree-node', function(e) {
+				var id = $(this).attr('data-id');
+
+				if (!$(e.target).hasClass('tree-hit')) {
+					self.selectionNode(target, id);
+				}
+
+				self.clickRow(e, this, target);
 			});
 
 			// 节点行事件监听
-			$(target).on('click', '.tree-node', function(e) {
-				self.clickRow(e, this, target);
+			$(target).on('click', '.tree-checkbox', function(e) {
+				self.toggleCheck(e, this);
 			});
 
 			// 展开收起图标事件监听
@@ -68,49 +73,79 @@
 		toggleNode: function(e, that, target) {
 			var self = this,
 				opts = $.data(target, 'tree').options,
-				node = self.findNode(target, that);
+				node = self.getNode(target, that);
 
 			if ($(that).hasClass(opts.collapseCls)) {
-				$(that).removeClass(opts.collapseCls);
-				$(that).addClass(opts.expandCls);
-				$(that).parent().next().slideDown();
-				if (opts.folderOpenCls !== opts.folderCls) {
-					$(that).parent().find('.tree-icon').addClass(opts.folderOpenCls);
-				}
+				self.expandNode(target, that);
 				if (typeof opts.onExpanded === 'function') {
 					opts.onExpanded.apply(that, [e, node]);
 				}
 			} else {
-				$(that).addClass(opts.collapseCls);
-				$(that).removeClass(opts.expandCls);
-				$(that).parent().next().slideUp();
-				if (opts.folderOpenCls !== opts.folderCls) {
-					$(that).parent().find('.tree-icon').removeClass(opts.folderOpenCls);
-				}
+				self.collapseNode(target, that);
 				if (typeof opts.onCollapsed === 'function') {
 					opts.onCollapsed.apply(that, [e, node]);
 				}
 			}
 		},
 
-		selectRow: function(that, target) {
+		toggleCheck: function(e, that) {
+			var self = this;
+
+			if ($(that).hasClass('tree-checkbox0')) {
+				$(that).removeClass('tree-checkbox0');
+				$(that).addClass('tree-checkbox1');
+			} else {
+				$(that).removeClass('tree-checkbox1');
+				$(that).addClass('tree-checkbox0');
+			}
+		},
+
+		expandNode: function(target, that) {
 			var self = this,
-				id, node,
 				opts = $.data(target, 'tree').options;
 
-			self.clearAllSelection(target);
-
-			if (typeof that === 'string' || typeof that === 'number') {
-				id = that;
-				$(target).find('[data-id=' + that + ']').addClass('tree-node-selected');
-			} else {
-				id = $(that).parent().attr('data-id');
-				$(that).parent().addClass('tree-node-selected');
+			$(that).removeClass(opts.collapseCls);
+			$(that).addClass(opts.expandCls);
+			$(that).parent().next().slideDown();
+			if (opts.folderOpenCls !== opts.folderCls) {
+				$(that).parent().find('.tree-icon').addClass(opts.folderOpenCls);
 			}
+		},
+
+		collapseNode: function(target, that) {
+			var self = this,
+				opts = $.data(target, 'tree').options;
+
+			$(that).addClass(opts.collapseCls);
+			$(that).removeClass(opts.expandCls);
+			$(that).parent().next().slideUp();
+			if (opts.folderOpenCls !== opts.folderCls) {
+				$(that).parent().find('.tree-icon').removeClass(opts.folderOpenCls);
+			}
+		},
+
+		selectionNode: function(target, id) {
+			var self = this,
+				node = self.getNodeById(target, id),
+				opts = $.data(target, 'tree').options,
+				$node = $(target).find('[data-id=' + id + ']');
+
+			self.clearAllSelection(target);
+			$node.addClass('tree-node-selected');
 
 			if (typeof opts.onSelectedNode === 'function') {
-				node = self.getNode(opts.data, id);
 				opts.onSelectedNode.apply(self, [node]);
+			}
+		},
+
+		selectionNodes: function(target, ids) {
+			var self = this,
+				$node,
+				ids = $.isArray(ids) ? ids : [];
+
+			for (var i = 0; i < ids.length; i++) {
+				$node = $(target).find('[data-id=' + ids[i] + ']');
+				$node.addClass('tree-node-selected');
 			}
 		},
 
@@ -120,7 +155,7 @@
 				opts = $.data(target, 'tree').options;
 
 			if (typeof opts.onClickNode === 'function') {
-				node = self.findNode(target, that);
+				node = self.getNode(target, that);
 
 				if (opts.onClickNode.apply(that, [e, node]) === false) {
 					e.preventDefault();
@@ -134,7 +169,7 @@
 				opts = $.data(target, 'tree').options;
 
 			if (typeof opts.onClickRow === 'function') {
-				node = self.findNode(target, that);
+				node = self.getNode(target, that);
 
 				if (opts.onClickRow.apply(that, [e, node]) === false) {
 					e.preventDefault();
@@ -142,51 +177,39 @@
 			}
 		},
 
-		loadData: function(url) {
-			var self = this;
+		loadData: function(target, url) {
+			var self = this,
+				opts = $.data(target, 'tree').options;
 
 			$.ajax({
 				url: url,
 				type: 'GET',
-				success: function(data) {
-					self.render(data);
+				cache: opts.cache,
+				beforeSend: opts.onAjaxBeforeSend,
+				complete: opts.onAjaxComplete,
+				error: opts.onAjaxError,
+				timeout: opts.timeout,
+				success: function(result) {
+					self.finishLoaded(target, result);
 				}
 			});
 		},
 
-		findNode: function(target, that) {
+		finishLoaded: function(target, result) {
 			var self = this,
-				id = $(that).parent().attr('data-id'),
-				opts = $.data(target, 'tree').options,
-				node = self.getNode(opts.data, id);
+				opts = $.data(target, 'tree').options;
 
-			return node;
+			opts.data = result.data;
+			self.buildData(opts.data, 0, [], opts.nodePathList);
+			self.render(target, opts.data);
 		},
 
-		getNode: function(data, id) {
-			if (data.length === 0) return;
-
+		getNode: function(target, that) {
 			var self = this,
-				node, result,
-				nodes = [];
+				id = $(that).parent().attr('data-id'),
+				node = self.getNodeById(target, id);
 
-			for (var i = 0; i < data.length; i++) {
-				node = {};
-				if (data[i].id == id) {
-					for (key in data[i]) {
-						if (key !== 'children') node[key] = data[i][key];
-					}
-					nodes.push(node);
-				}
-
-				result = self.getNode(data[i].children || [], id);
-
-				if (result) {
-					nodes.push(result);
-				}
-			}
-
-			return nodes.length ? nodes[0] : null;
+			return node;
 		},
 
 		getNodes: function(data, ids) {
@@ -237,26 +260,20 @@
 			return nodes.length ? nodes[0] : null;;
 		},
 
-		getFirstLeafNode: function(data) {
-			if (data.length === 0) return [];
-
+		expandTo: function(target, id) {
 			var self = this,
-				nodes = [],
-				result,
-				item = data[0],
-				children = item.children || [];
+				that, parentNode,
+				opts = $.data(target, 'tree').options,
+				node = self.getNodeById(target, id),
+				level = node.level,
+				nodePath = node.nodePath;
 
-			if (children.length > 0) {
-				result = self.getFirstLeafNode(children);
-
-				if (result) {
-					nodes.push(result);
-				}
-			} else {
-				nodes.push(item)
+			while (level > 1) {
+				nodePath = nodePath.slice(0, --level);
+				parentNode = self.getNodeByPath(nodePath, opts.data);
+				that = $(target).find('[data-id=' + parentNode.id + ']').find('.tree-hit').get(0);
+				self.expandNode(target, that);
 			}
-
-			return nodes.length ? nodes[0] : null;;
 		},
 
 		expandAll: function(target) {
@@ -301,8 +318,8 @@
 				prevNode = self.getPrevNode(target, id, isSelectionFolder);
 
 				if (prevNode) {
-					self.selectRow(prevNode.id, target);
-
+					self.selectionNode(target, prevNode.id);
+					self.expandTo(target, prevNode.id);
 					return true;
 				}
 			}
@@ -320,8 +337,8 @@
 				nextNode = self.getNextNode(target, id, isSelectionFolder);
 
 				if (nextNode) {
-					self.selectRow(nextNode.id, target);
-
+					self.selectionNode(target, nextNode.id);
+					self.expandTo(target, nextNode.id);
 					return true;
 				}
 			}
@@ -335,7 +352,7 @@
 				prevLeafNode,
 				opts = $.data(target, 'tree').options,
 				data = opts.data,
-				node = self.getNode(data, id),
+				node = self.getNodeById(target, id),
 				level = node.level,
 				parent = false,
 				nodePath = $.extend([], node.nodePath);
@@ -343,12 +360,12 @@
 			while (!siblingNode && level > -1) {
 				nodePath[level - 1] = nodePath[level - 1] - 1;
 
-				if (self.getPathNode(nodePath, data)) {
-					siblingNode = self.getPathNode(nodePath, data);
+				if (self.getNodeByPath(nodePath, data)) {
+					siblingNode = self.getNodeByPath(nodePath, data);
 				} else {
 					nodePath = nodePath.slice(0, --level);
 					if (isSelectionFolder) {
-						return self.getPathNode(nodePath, data);
+						return self.getNodeByPath(nodePath, data);
 					}
 				}
 			}
@@ -367,7 +384,7 @@
 				nextLeafNode,
 				opts = $.data(target, 'tree').options,
 				data = opts.data,
-				node = self.getNode(data, id),
+				node = self.getNodeById(target, id),
 				level = node.level,
 				nodePath = $.extend([], node.nodePath);
 
@@ -378,7 +395,7 @@
 				} else {
 					nodePath.push(0);
 				}
-				nextLeafNode = self.getPathNode(nodePath, data);
+				nextLeafNode = self.getNodeByPath(nodePath, data);
 				if (nextLeafNode) {
 					node = nextLeafNode;
 					if (isSelectionFolder) {
@@ -396,7 +413,20 @@
 			return siblingNode;
 		},
 
-		getPathNode: function(path, data) {
+		getNodeById: function(target, id) {
+			var self = this,
+				node,
+				opts = $.data(target, 'tree').options,
+				nodePath = opts.nodePathList[id];
+
+			if (nodePath) {
+				node = self.getNodeByPath(nodePath, opts.data);
+			}
+
+			return node;
+		},
+
+		getNodeByPath: function(path, data) {
 			var self = this,
 				node, i = 0;
 
@@ -449,11 +479,20 @@
 			}
 		},
 
-		render: function(data, target, opts) {
+		render: function(target, data) {
 			var self = this,
+				opts = $.data(target, 'tree').options,
 				html = self.getTreeHtml(data, opts);
 
 			$(target).html(html);
+
+			if (opts.expandAll) {
+				self.expandAll(target);
+			}
+
+			if (typeof opts.onAfterRender === 'function') {
+				opts.onAfterRender.apply(self, []);
+			}
 		},
 
 		getTreeHtml: function(data, opts) {
@@ -498,6 +537,14 @@
 				html.push('<span class="tree-icon ' + opts.leafCls + '"></span>');
 			}
 
+			if (typeof item.checked !== 'undefined') {
+				if (item.checked) {
+					html.push('<span class="tree-checkbox tree-checkbox1"></span>');
+				} else {
+					html.push('<span class="tree-checkbox tree-checkbox0"></span>');
+				}
+			}
+
 			// 添加节点的文本
 			if (item.url) {
 				html.push('<a class="tree-text link" target="_blank;" href=' + item.url + '>' + item.text + '</a>');
@@ -530,6 +577,21 @@
 
 	// 公用方法对外面可见
 	$.fn.tree.methods = {
+		expandTo: function(jq, id) {
+
+			tree.expandTo(jq[0], id);
+		},
+
+		selectionNode: function(jq, id) {
+
+			tree.selectionNode(jq[0], id);
+		},
+
+		selectionNodes: function(ids) {
+
+			tree.selectionNodes(jq[0], ids);
+		},
+
 		expandAll: function(jq) {
 
 			tree.expandAll(jq[0]);
@@ -538,6 +600,7 @@
 
 			tree.collapseAll(jq[0]);
 		},
+
 		prevNodeSelection: function(jq, isSelectionFolder) {
 
 			return tree.prevNodeSelection(jq[0], isSelectionFolder);
@@ -566,8 +629,14 @@
 		folderCls: 'tree-folder',
 		folderOpenCls: 'tree-folder-open',
 		hasNodeIcon: true,
+		expandAll: false,
+		cache: false,
+		timeout: 60000,
 		queryParams: {},
 		nodePathList: {},
+		onAjaxBeforeSend: function() {},
+		onAjaxComplete: function() {},
+		onAjaxError: function() {},
 		onClickNode: function(e, node) {},
 		onClickRow: function(e, ndoe) {},
 		onExpanded: function(e, node) {},
@@ -575,5 +644,6 @@
 		onSelectedNode: function(node) {},
 		onExpandedAll: function() {},
 		onCollapseAll: function() {},
+		onAfterRender: function() {}
 	};
 })(jQuery);
