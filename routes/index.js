ce6b275f94c22635822b993d4d20794096b869d4
weiyao86 <weiyaoisnull@gmail.com>
2016-08-25 20:17:46 +0800

    待修复BUG－combobox

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var comboList = require('../public/data/combobox.json');
 
 router.get('/', function(req, res, next) {
 	res.render('index.ejs');
@@ -19,6 +20,63 @@ router.get('/combobox', function(req, res, next) {
 
 router.get('/window', function(req, res, next) {
 	res.render('window.ejs');
+});
+
+router.get('/getComboList/brand', function(req, res, next) {
+	var q = req.query,
+		rst = {
+			Data: []
+		};
+
+	if (!q.q) {
+		return res.json({
+			Data: comboList.Brand
+		});
+	}
+
+
+	var hash = {},
+		arr = [];
+
+	comboList.Brand.forEach(function(val, idx) {
+		q.q.forEach(function(v, i) {
+			if (!v.toLowerCase() || val.Description.toLowerCase().indexOf(v.toLowerCase()) > -1) {
+				arr.push(val);
+			}
+		});
+	});
+
+	arr.length && arr.forEach(function(v, i) {
+		if (!hash[v.Code]) {
+			hash[v.Code] = true;
+			rst.Data.push(v);
+		}
+	});
+
+	res.json(rst);
+});
+
+router.get('/getComboList/series', function(req, res, next) {
+	var q = req.query,
+		rst = {
+			Data: []
+		};
+
+	if (!q.parentId) return rst;
+	if (!q.q || !q.q.length) return res.json({
+		Data: comboList.Series
+	});
+	q.parentId.forEach(function(pv, pi) {
+		q.q.forEach(function(v, i) {
+			comboList.Series.forEach(function(val, idx) {
+				if ((pv.toLowerCase() == val.BrandCode.toLowerCase()) && (!v.toLowerCase() || val.Description.toLowerCase().indexOf(v.toLowerCase()) > -1)) {
+					console.log(val.BrandCode.toLowerCase());
+					rst.Data.push(val);
+				}
+			});
+		});
+	});
+	res.json(rst);
 });
 
 module.exports = router;
